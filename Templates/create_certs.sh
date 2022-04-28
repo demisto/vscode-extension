dbbf34a33c2bca4bc4eeb3495c1bf2d2eed8b305
ilan <ierukhimovic@paloaltonetworks.com>
2022-04-28 11:07:21 +0300

    documantation

--- Templates/create_certs.sh
+++ Templates/create_certs.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# This script creates a local certificate file.
+# It tries to connect to a server (for instance, github), show the local certificates and save it to a file.
+
+
 # We connect to a random server and not paloaltonetworks.com to get external certificates. 
 CONNECT_SERVER="github.com:443"
 
@@ -9,7 +13,7 @@ REGEX_BEGIN="/^-----BEGIN CERTIFICATE-----$/"
 REGEX_END="/^-----END CERTIFICATE-----$"
 
 # Parse the certificate to a file
-openssl s_client -showcerts  -connect $CONNECT_SERVER | \
+openssl s_client -showcerts -connect $CONNECT_SERVER | \
     sed -n "$REGEX_BEGIN,$REGEX_END/p" > "$FILE"
 
 if [ ! -f "$FILE" ]; then
