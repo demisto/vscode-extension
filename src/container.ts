ba19b7bbd6878fb3b3e396ea4135cfc6b1120ae6
ilan <ierukhimovic@paloaltonetworks.com>
2022-06-21 13:50:48 +0300

    Revert "Revert "update from last commit""
    This reverts commit 04074c5e93528c66556284db521af24f0bc1c19e.

--- src/container.ts
+++ src/container.ts
@@ -10,31 +10,25 @@ import * as fs from "fs-extra";
 export async function createIntegrationDevContainer(fileName: string): Promise<void> {
     const devcontainerFolder = path.join(fileName, '.devcontainer')
     Logger.info(`devcontainerFolder is ${devcontainerFolder}`)
-    const filePath = path.parse(fileName)
-    const ymlFilePath = path.join(fileName, filePath.name.concat('.yml'))
-    const ymlObject = yaml.parseDocument(fs.readFileSync(ymlFilePath, 'utf8')).toJSON();
-    const dockerImage = ymlObject.dockerimage || ymlObject?.script.dockerimage
-    Logger.info(`docker image is ${dockerImage}`)
     if (!await fs.pathExists(path.join(devcontainerFolder, 'devcontainer.json'))) {
         vscode.window.showInformationMessage("Starting demisto-sdk lint, please wait")
         await dsdk.lint(fileName, false, false, true)
         vscode.window.showInformationMessage("Building devcontainer folder")
-        const devcontainerJsonPath = path.resolve(__dirname, '../Templates/integration_env/.devcontainer/devcontainer.json')
+        const filePath = path.parse(fileName)
+        const ymlFilePath = path.join(fileName, filePath.name.concat('.yml'))
+        const ymlObject = yaml.parseDocument(fs.readFileSync(ymlFilePath, 'utf8')).toJSON();
+        const dockerImage = ymlObject.dockerimage || ymlObject?.script.dockerimage
+        Logger.info(`docker image is ${dockerImage}`)
+        const devcontainerJsonPath = path.resolve(__dirname, '../Templates/.devcontainer/devcontainer.json')
         const devcontainer = JSON.parse(fs.readFileSync(devcontainerJsonPath, 'utf-8'))
         devcontainer.build.args.IMAGENAME = dockerImage
-        fs.copySync(path.resolve(__dirname, '../Templates/integration_env/.devcontainer'), devcontainerFolder)
+        fs.copySync(path.resolve(__dirname, '../Templates/.devcontainer'), devcontainerFolder)
         fs.writeJSONSync(path.join(devcontainerFolder, 'devcontainer.json'), devcontainer)
         Logger.info('devcontainer folder created')
         const cmd = `sh -x ${path.resolve(__dirname, '../Templates/create_certs.sh')} ${path.join(devcontainerFolder, 'certs.crt')}`
         Logger.info(cmd)
         execSync(cmd, { cwd: fileName })
         Logger.info('certs.crt created, now creating container')
-    }
-    else {
-        Logger.info(`devcontainer folder exists. Updating Image to ${dockerImage}`)
-        const devcontainer = JSON.parse(fs.readFileSync(path.join(devcontainerFolder, 'devcontainer.json'), 'utf-8'))
-        devcontainer.build.args.IMAGENAME = dockerImage
-        fs.writeJSONSync(path.join(devcontainerFolder, 'devcontainer.json'), devcontainer)
     }
     Logger.info(`remote name is: ${vscode.env.remoteName}`)
     Logger.info(`fileName is: ${fileName}`)
@@ -49,7 +43,7 @@ export async function createIntegrationDevContainer(fileName: string): Promise<v
         Logger.debug(`relative pack is ${reltaveFileName}`)
         const localFileName = path.join(local_workspace_path, reltaveFileName)
         Logger.debug(`local file path is ${localFileName}`)
-        fileNameUri = vscode.Uri.parse(`vscode://${localFileName}`)
+        fileNameUri = vscode.Uri.parse(`vscode://${fileName}`)
     }
     else if (vscode.env.remoteName === "wsl") {
         fileNameUri = vscode.Uri.parse(`vscode://${fileName}`)
@@ -59,7 +53,7 @@ export async function createIntegrationDevContainer(fileName: string): Promise<v
         vscode.window.showErrorMessage('Please install remote-containers extension to use this feature')
     }
     else {
-        vscode.commands.executeCommand('remote-containers.openFolder', fileNameUri, true)
+        vscode.commands.executeCommand('remote-containers.openFolder', fileNameUri)
     }
 
 
@@ -73,10 +67,13 @@ export async function createContentDevContainer(): Promise<void> {
     const workspaceFolder = workspaceFolders[0]
     const devcontainerFolder = path.join(workspaceFolder.uri.fsPath, '.devcontainer')
     Logger.info(`devcontainerFolder is ${devcontainerFolder}`)
-    fs.copySync(path.resolve(__dirname, '../Templates/content_env/.devcontainer'), devcontainerFolder)
-    const cmd = `sh -x ${path.resolve(__dirname, '../Templates/create_certs.sh')} ${path.join(devcontainerFolder, 'certs.crt')}`
-    Logger.info(cmd)
-    execSync(cmd, { cwd: devcontainerFolder })
+    if (!await fs.pathExists(path.join(devcontainerFolder, 'devcontainer.json'))) {
+        fs.copySync(path.resolve(__dirname, '../Templates/.devcontainer'), devcontainerFolder)
+        fs.copySync(path.resolve(__dirname, '../Templates/devcontainer.json'), path.join(devcontainerFolder, 'devcontainer.json'))
+        const cmd = `sh -x ${path.resolve(__dirname, '../Templates/create_certs.sh')} ${path.join(devcontainerFolder, 'certs.crt')}`
+        Logger.info(cmd)
+        execSync(cmd, { cwd: devcontainerFolder })
+    }
     if (!(await vscode.commands.getCommands()).includes('remote-containers.openFolder')) {
         vscode.window.showErrorMessage('Please install remote-containers extension to use this feature')
     }
