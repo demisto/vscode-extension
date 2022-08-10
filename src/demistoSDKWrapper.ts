a663631fd6cd96649560f5c2aade5afc1c87b489
israelpolishook <ipolishuk@paloaltonetworks.com>
2022-08-10 16:31:09 +0300

    Reimplementation of the Run command

--- src/demistoSDKWrapper.ts
+++ src/demistoSDKWrapper.ts
@@ -44,10 +44,14 @@ export function formatCommand(file: string): void {
 	const command = ['format', '-i', file];
 	TerminalManager.sendDemistoSDKCommand(command);
 }
-export function uploadToXSOAR(file: string): void {
+export async function uploadToXSOAR(file: string, progress = false): Promise<void> {
 	const command = ['upload', '-i', file];
-	TerminalManager.sendDemistoSDKCommand(command);
-
+	if (progress) {
+		await TerminalManager.sendDemistoSdkCommandWithProgress(command)
+	}
+	else {
+		TerminalManager.sendDemistoSDKCommand(command);
+	}
 
 }
 export function lintUsingGit(file: string): void {
@@ -71,90 +75,252 @@ export async function lint(file: string, tests = true, lints = true, progress = 
 	}
 }
 
+/*************************** 
+	  RUN   COMMMAND
+****************************/
+
 interface Argumnet {
 	name: string
 	description: string
-	required: string
+	required: string | boolean
 	predefined: string[]
 }
+
 interface Command {
 	name: string
 	description: string
 	arguments: Argumnet[]
 }
 
-export function run(dirPath: string): void {
+function quickPickItemForCommands(_list: Command[]): vscode.QuickPickItem[] {
+	if (_list.length > 0) {
+		return _list.map((command: Command) => {
+			return { label: command.name, detail: command.description }
+		})
+	}
+	else {
 
-	const filePath = path.parse(dirPath)
-	const ymlFilePath = path.join(dirPath, filePath.name.concat('.yml'))
-	const ymlObject = yaml.parseDocument(fs.readFileSync(ymlFilePath, 'utf8')).toJSON();
-	const commandNames: vscode.QuickPickItem[] = ymlObject.script.commands.map((command: Command) => {
-		return { label: command.name, description: command.description }
+		return []
+	}
+}
+
+function QuickPickItemForArgs(_list: Argumnet[]): [vscode.QuickPickItem[], string[]] {
+
+	const argsRequired: string[] = []
+	const argsList: vscode.QuickPickItem[] = _list.map((arg: Argumnet) => {
+		if (arg.required === "true" || arg.required === true) {
+			arg.required = "required";
+			argsRequired.push(arg.name)
+		}
+		else {
+			arg.required = "optional"
+		}
+		return { label: arg.name, detail: arg.description, description: arg.required }
 	})
+	// Order the argument list so that required is at the beginning
+	return [argumentSorted(argsList), argsRequired]
+}
+
+function QuickPickItemForPredefined(_list: string[]): vscode.QuickPickItem[] | undefined {
+	if (_list.length > 0) {
+		return _list.map((item: string) => {
+			return { label: item }
+		})
+	}
+}
+
+function argumentSorted(_list: vscode.QuickPickItem[]): vscode.QuickPickItem[] {
+	if (_list.length > 0) {
+		_list.sort((arg1: vscode.QuickPickItem, arg2: vscode.QuickPickItem) => {
+			if (arg1.description == "required" && arg2.description != "required") {
+				return -1;
+			}
+			if (arg1.description != "required" && arg2.description == "required") {
+				return 1;
+			}
+			return 0
+		})
+		return _list
+	}
+	return _list
+}
+
+function runCommand(query: Map<string, string>) {
+	let cmd = `"${query.get('cmd')}`
+	if (typeof cmd === 'string') {
+		query.delete('cmd')
+		const queryTest = Array.from(query.entries())
+		queryTest.map((arg: [string, string]) => {
+			cmd += ` ${arg[0]}=${arg[1]}`
+		})
+		const q: string[] = ['run', '-q', cmd + `"`]
+		TerminalManager.sendDemistoSDKCommand(q);
+	}
+}
+
+async function showCommandsOrArguments(commandNames: vscode.QuickPickItem[]): Promise<vscode.QuickPickItem | undefined> {
+
+	const commandName = await vscode.window.showQuickPick(
+		commandNames, {
+		title: "Choose your command",
+		placeHolder: "The command to run with XSOAR"
+	}
+	)
+	return commandName
+
+}
+
+async function argsManagment(argumentNames: vscode.QuickPickItem[], args: Argumnet[], argsRequired: string[],
+	query: Map<string, string>): Promise<[Map<string, string>, boolean]> {
+
+	let flag = false
+	return await showCommandsOrArguments(argumentNames).then(async (arg) => {
+		if (arg && arg.label != "Run command") {
+			const argument = args.find(x => x.name == arg.label)
+			if (argument?.predefined) {
+				const predefined: vscode.QuickPickItem[] | undefined = QuickPickItemForPredefined(argument.predefined)
+				if (predefined) {
+					await vscode.window.showQuickPick(
+						predefined
+					).then((value) => {
+						if (value && value.label) {
+							query.set(arg.label, value.label)
+						}
+					})
+				}
+			}
+			else {
+				await vscode.window.showInputBox(
+					{
+						placeHolder: arg.description
+					}
+				).then(
+					async (value) => {
+						if (value) {
+							query.set(arg.label, value)
+						}
+					}
+				)
+			}
+			return [query, flag]
+		}
+		else {
+			const argsRequiredMissing: string[] = []
+			if (argsRequired.length > 0) {
+				argsRequired.map((argName: string) => {
+					if (query.has(argName)) {
+						console.log(argName)
+					}
+					else {
+						argsRequiredMissing.push(argName)
+					}
+				})
+			}
+			if (argsRequiredMissing.length > 0) {
+				let message = ''
+				argsRequiredMissing.map((arg: string) => {
+					message += '\n' + arg
+				})
+				const header = 'The following arguments are required:'
+				message += '\n' + arg
+				await vscode.window.showInformationMessage(header, { modal: true, detail: message }, "Back").then(async (response) => {
+					if (response != "Back") {
+						flag = true
+						return [query, flag]
+					}
+					else {
+						return [query, flag]
+					}
+				})
+			}
+			else {
+				flag = true
+				return [query, flag]
+			}
+		}
+	}).then(() => { return [query, flag] })
+}
+
+async function runIntegration(ymlObject: any): Promise<Map<string, string>> {
+	// Map for query the command
+	const query: Map<string, string> = new Map<string, string>()
+	// Map contain all of the integration's commands
+	const commandNames: vscode.QuickPickItem[] | undefined = quickPickItemForCommands(ymlObject.script.commands)
 	const commands: Map<string, Command> = new Map<string, Command>()
 	ymlObject.script.commands.forEach((command: Command) => { commands.set(command.name, command) })
-	if (ymlFilePath) {
-		const query : Map<string, string> = new Map<string, string>()
-		vscode.window.showQuickPick(
-			commandNames, {
-			title: "Choose your command",
-			placeHolder: "The command to run with XSOAR"
-		}
-		).then(
-			(commandName) => {
-				if (commandName) {
-					const command = commands.get(commandName.label)
-					if (command) {
-						query.set('cmd', command.name)
-						const argumentNames: vscode.QuickPickItem[] = command.arguments.map((arg: Argumnet) => {
-							return {label: arg.name, description: arg.description}
+
+	return await showCommandsOrArguments(commandNames).then(async (commandName) => {
+		if (commandName) {
+			const command = commands.get(commandName.label)
+			if (command) {
+				query.set('cmd', command.name)
+				const [argumentNames, argsRequired] = QuickPickItemForArgs(command.arguments)
+				if (argumentNames) {
+					argumentNames.unshift({ label: "Run command", detail: "After you've finished filling in the arguments, or no arguments have been consumed" })
+					let flag = true
+					while (flag == true) {
+						await argsManagment(argumentNames, command.arguments, argsRequired, query).then(async ([queryForRun, ForRun]) => {
+							if (ForRun == true) {
+								flag = false
+								console.log(`${queryForRun}`)
+							}
 						})
-						let i = 1 as number
-						while (i === 1){
-							vscode.window.showQuickPick(
-								argumentNames,{
-									title: "Choose the argument",
-									placeHolder: ""
-								}
-							).then(
-								(arg) => {
-									if (arg){
-									vscode.window.showInputBox(
-										{
-											placeHolder: arg.description
-										}
-									).then(
-										(value) => {
-											if (value) {
-												query.set(arg.label, value)
-											}
-											i = 2
-										}
-									)
-								}}
-							)
-						}
 					}
 				}
 			}
-		)
-		TerminalManager.sendText("echo Israel", true)
-	}
-	// vscode.window.showInputBox(
-	// 	{
-	// 		value: "Command to run"
-	// 	}
-	// ).then(
-	// 	(value) => {
-	// 		if (value) {
-	// 			value = JSON.stringify(value)
-	// 			const command = ['run', '-q', value];
-	// 			TerminalManager.sendDemistoSDKCommand(command);
-	// 		}
-	// 	});
+		}
+	}).then(() => { return query })
+}
 
+async function runScript(ymlObject: any): Promise<Map<string, string>> {
+	const query: Map<string, string> = new Map<string, string>()
+	const [argumentNames, argsRequired] = QuickPickItemForArgs(ymlObject.args)
+	query.set('cmd', ymlObject.name)
+
+	argumentNames.unshift({ label: "Run command", detail: "After you've finished filling in the arguments, or no arguments have been consumed" })
+	let flag = true
+	const a = async () => {
+		while (flag == true) {
+			await argsManagment(argumentNames, ymlObject.args, argsRequired, query).then(async ([queryForRun, ForRun]) => {
+				if (ForRun == true) {
+					flag = false
+					console.log(`${queryForRun}`)
+				}
+			})
+		}
+		return query
+	}
+
+	return await a()
 
 }
+
+
+export async function run(dirPath: string): Promise<void> {
+
+	return await vscode.window.showInformationMessage('Do you want uploading before running?', { modal: true }, 'YES', 'NO').then(async (upload) => {
+		if (upload) {
+			if (upload === 'YES') {
+				await uploadToXSOAR(dirPath, true)
+			}
+		}
+	}).then(async () => {
+		const filePath = path.parse(dirPath)
+		const ymlFilePath = path.join(dirPath, filePath.name.concat('.yml'))
+		const ymlObject = yaml.parseDocument(fs.readFileSync(ymlFilePath, 'utf8')).toJSON()
+		let queryMap: Map<string, string>
+		if ("configuration" in ymlObject) {
+			queryMap = await runIntegration(ymlObject)
+		}
+		else {
+			queryMap = await runScript(ymlObject)
+		}
+		if (queryMap) {
+			runCommand(queryMap)
+		}
+	})
+}
+
 
 interface demistoSDKReport {
 	"filePath": string,
